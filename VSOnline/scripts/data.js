50087f57a16d8943c1585fce35d294b05b647c7d
Jim Lamb <jim@jflamb.com>
2013-04-04 17:42:55 -0400

    Integrated jQuery templates for generating recent section content.

---
+++ VSOnline/scripts/data.js
@@ -0,0 +1,36 @@
+var recentItems = [
+    {
+        "group": "Today",
+        "items": [
+            {
+                "symbol": "&#xE188",
+                "type": "Project",
+                "name": "Rapid Rain",
+                "description": "7 members"
+            },
+            {
+                "symbol": "&#xE125",
+                "type": "Team",
+                "name": "Freezing Fog / Front-end",
+                "description": "5 member team"
+            }
+        ]
+    },
+    {
+        "group": "Yesterday",
+        "items": [
+            {
+                "symbol": "&#xE188",
+                "type": "Project",
+                "name": "Sudden Storm",
+                "description": "11 members"
+            },
+            {
+                "symbol": "&#x1F4AC",
+                "type": "Room",
+                "name": "Freezing Fog Team Room",
+                "description": "Who can work on this bug?"
+            }
+        ]
+    }
+];
